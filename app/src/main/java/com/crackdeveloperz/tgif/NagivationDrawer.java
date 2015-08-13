a74d682ddb4fa47900832f6622df87d560076f62
javanux <javanuxenergy@gmail.com>
2015-08-13 22:03:23 +0545

    minor changes

--- app/src/main/java/com/crackdeveloperz/tgif/NagivationDrawer.java
+++ app/src/main/java/com/crackdeveloperz/tgif/NagivationDrawer.java
@@ -2,7 +2,6 @@ package com.crackdeveloperz.tgif;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
@@ -11,10 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 
 /**
